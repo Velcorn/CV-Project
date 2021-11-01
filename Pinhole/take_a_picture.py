b522833b52467884ca0e77c2d92a7d2e55970f5a
Velcorn <Velcorn@users.noreply.github.com>
2021-11-01 18:30:41 +0100

    Finished pinhole camera exercise

--- Pinhole/take_a_picture.py
+++ Pinhole/take_a_picture.py
@@ -1,13 +1,13 @@
 import numpy as np
 import imageio
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+# import matplotlib.image as mpimg
 
 # camera intrinsic
-fx = 1066.778;
-fy = 1066.778;
-cx = 312.9869;
-cy = 241.3109;
+fx = 1066.778
+fy = 1066.778
+cx = 312.9869
+cy = 241.3109
 intrinsic_matrix = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
 
 # transformation matrix
@@ -17,13 +17,13 @@ point_pose = np.hstack([point_rotation, point_translation.T])
 
 # 3D point
 point_3D = np.loadtxt("point_3D.txt")
-point_3D = point_3D[:,0:3]
-point_3D_homogeneous = ...
-point_transformation = ...
+point_3D = point_3D[:, 0:3]
+point_3D_homogeneous = np.hstack([point_3D, np.ones((point_3D.shape[0], 1))])
+point_transformation = np.hstack([point_rotation, point_translation.T])
 
 # project to 2D point
-point_2D_homogeneous = ...
-point_2D = ...
+point_2D_homogeneous = np.dot(point_transformation, point_3D_homogeneous.T)
+point_2D = np.dot(intrinsic_matrix, point_2D_homogeneous)
 
 # visualization
 img = imageio.imread('000001-color.png')
